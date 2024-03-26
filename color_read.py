9281d118795304d8fd76e7c8fdcceef7d42ea6c7
sonamdev <dangisonam80@gmail.com>
2024-03-27 00:46:30 +0530

    Color detection project

---
+++ color_read.py
@@ -0,0 +1,9 @@
+import cv2 
+import numpy as np
+import pandas as pd
+import argparse # to parse command line arguments
+ap=argparse.ArgumentParser()
+ap.add_argument('-i','--image',required=True, help="image path")
+
+
+
